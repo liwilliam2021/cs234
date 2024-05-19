414f516085566e02c1f654e79b9b2712d847c966
pongnguy <alfred.wechselberger@gmail.com>
2024-05-19 23:52:49 +0000

    various

--- alfred/test.py
+++ alfred/test.py
@@ -1,11 +1,11 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
-tokenizer = AutoTokenizer.from_pretrained(r"dmayhem93/toolformer_v0_epoch2")
+tokenizer = AutoTokenizer.from_pretrained(r"dmayhem93/toolformer_v0_epoch2", cache_dir='/root/.cache/huggingface/hub')
 model = AutoModelForCausalLM.from_pretrained(
     r"dmayhem93/toolformer_v0_epoch2",
     torch_dtype=torch.float16,
-    low_cpu_mem_usage=True,
+    low_cpu_mem_usage=True, cache_dir='/root/.cache/huggingface/hub'
 ).cuda()
 generator = pipeline(
     "text-generation", model=model, tokenizer=tokenizer, device=0
