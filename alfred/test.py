3ff8e51184a2e6cf5f466ca17d34056c70c56150
alfred <alfred.wechselberger@gmail.com>
2024-05-20 00:45:58 -0400

    initial

---
+++ alfred/test.py
@@ -0,0 +1,23 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from toolformer.data_generator import DataGenerator
+from toolformer.api import CalculatorAPI
+from toolformer.prompt import calculator_prompt
+from toolformer.utils import yaml2dict
+
+config = yaml2dict('../configs/default.yaml')
+calculator_api = CalculatorAPI(
+    "Calculator", calculator_prompt,
+    sampling_threshold=0.2, filtering_threshold=0.2
+)
+
+model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-560m")
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
+
+text = "From this, we have 10 - 5 minutes = 5 minutes."
+apis = [calculator_api]
+generator = DataGenerator(config, model, tokenizer, apis=apis)
+
+augumented_text_ids = generator.generate(text)
+
+print(tokenizer.decode(augumented_text_ids[0][0], skip_special_tokens=True))
