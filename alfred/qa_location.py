ff53b240368fe3ade8737cdc4ef8936901bb9138
alfred <alfred.wechselberger@gmail.com>
2024-05-22 23:15:59 +0000

    various

--- alfred/qa_location.py
+++ alfred/qa_location.py
@@ -7,8 +7,9 @@ from toolformer.prompt import qa_prompt
 from toolformer.utils import yaml2dict
 
 config = yaml2dict('../configs/default.yaml')
+# Alfred: needs to look for QA based on the qa_prompt which was given
 location_api = LocationAPI(
-    "Location", qa_prompt, cities_csv='uscities.csv',
+    "QA", qa_prompt, cities_csv='uscities.csv',
     sampling_threshold=0.2, filtering_threshold=0.2
 )
 
@@ -18,7 +19,7 @@ model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-560m")
 tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
 
 #text = "What is the temperature in Baltimore, MD?"
-text = "From Baltimore, MD we have that Baltimore is in the state of Maryland"
+text = "From Baltimore, MD we have that Baltimore is in the state of Maryland."
 #text = "39.2896246543727, -76.58026446823449"  # Patterson Park, Baltimore, MD
 #text = "From this, we have 10 - 5 minutes = 5 minutes."
 apis = [location_api]
